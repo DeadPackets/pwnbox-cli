0a47ba32e4d8803f1370c8d875ae3606dbbb5173
Github Action <action@github.com>
2021-09-05 08:35:58 +0000

    [GitHub Action] Update version info

--- pwnbox/__main__.py
+++ pwnbox/__main__.py
@@ -16,7 +16,7 @@ from rich.progress import BarColumn, Progress
 from ssh_wait import ssh_wait
 
 # Global Vars
-VERSION = "v1.5.3"
+VERSION = "v1.6.0"
 console = Console()
 
 # Bytes to Human Readable
